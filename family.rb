1a975ee43341434d9b9942e10bca9f0836e244b2
rallen-fiksu <rallen@fiksu.com>
2014-05-05 20:44:27 -0400

    begun creating the person class

--- family.rb
+++ family.rb
@@ -11,3 +11,23 @@ begin
 	people = gets.chomp.to_i
 end while (people < 1 || people > 10) && (people % 2 != 0)
 puts "You typed: #{people}."
+
+# people tells program how many people to start with in the first generation.
+# for testing purposes, we'll have each person be given a number which are added together in each new person.
+
+class Person
+	def initialize(name, gender, number)
+		@name = name
+		@gender = gender
+		@number = number
+	end
+
+	def id
+		puts "Hi! My name is #{name} and I am #{gender}. My number is #{number}"
+	end
+end
+
+p = guy1.new('Thorpe', 'Male', '1')
+puts "#{p.name}"
+puts "#{p.gender}"
+puts "#{p.number}"
