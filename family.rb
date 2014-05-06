a4248e11d61ad276feeefae94b54dcb461e41c4f
rallen-fiksu <rallen@fiksu.com>
2014-05-06 08:32:19 -0400

    went back to class model, trying to get arguments for new person to work

--- family.rb
+++ family.rb
@@ -15,7 +15,6 @@ puts "You typed: #{people}."
 # people tells program how many people to start with in the first generation.
 # for testing purposes, we'll have each person be given a number which are added together in each new person.
 
-=begin
 # Person as class
 class Person
 	attr_accessor :name, :gender, :number
@@ -30,8 +29,19 @@ class Person
 		puts "Hi! My name is #{name} and I am #{gender}. My number is #{number}"
 	end
 end
+
+man = Person.new("Thorpe", "Male", 1)
+man_n = man.number
+puts man_n
+=begin
+woman = Person.new('Jain', 'Female', '1')
+woman_n = woman.number
+puts woman_n
+child = Person.new('kid1', 'Male', '1')
+puts "#{child}"
 =end
 
+=begin
 #Person as Struct
 Person = Struct.new(:name, :gender, :number) do
   def id
@@ -45,5 +55,7 @@ puts man_n
 woman = Person.new('Jain', 'Female', '1')
 woman_n = woman.number
 puts woman_n
-child = Person.new('kid1', 'Male', '#{man_n}+#{woman_n}')
+child = Person.new('kid1', 'Male', '1')
 puts "#{child}"
+
+=end
