6085011a9b4edaf67cdaa11b4b96c2ec6c1a1a97
rallen-fiksu <rallen@fiksu.com>
2014-05-06 08:12:48 -0400

    touched up Person class, working on child person struct

--- family.rb
+++ family.rb
@@ -15,17 +15,35 @@ puts "You typed: #{people}."
 # people tells program how many people to start with in the first generation.
 # for testing purposes, we'll have each person be given a number which are added together in each new person.
 
+=begin
+# Person as class
 class Person
-	def initialize(name, gender, number)
-		@name = name
-		@gender = gender
-		@number = number
+	attr_accessor :name, :gender, :number
+
+	def initialize(opts = {})
+		@name = opts[:name]
+		@gender = opts[:gender]
+		@number = opts[:number]
 	end
 
 	def id
 		puts "Hi! My name is #{name} and I am #{gender}. My number is #{number}"
 	end
 end
+=end
 
-p = Person.new('Thorpe', 'Male', '1')
-puts "#{p}"
+#Person as Struct
+Person = Struct.new(:name, :gender, :number) do
+  def id
+    puts "Hi! My name is #{name} and I am #{gender}. My number is #{number}"
+  end
+end
+
+man = Person.new('Thorpe', 'Male', '1')
+man_n = man.number
+puts man_n
+woman = Person.new('Jain', 'Female', '1')
+woman_n = woman.number
+puts woman_n
+child = Person.new('kid1', 'Male', '#{man_n}+#{woman_n}')
+puts "#{child}"
