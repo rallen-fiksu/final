e5438c842106f65b120471435e1808ef840f2a87
rallen-fiksu <rallen@fiksu.com>
2014-05-12 19:40:14 -0400

    Hardcoded a simplistic view of my program. Plan on substituting the numbers for gene pairs. Once thats done, I will try to adapt this to a website

--- hard_code.rb
+++ hard_code.rb
@@ -21,20 +21,20 @@ class Person
 end
 
 # Generation 1
-generation1 = {:couple1 => [man_a1, woman_a1], :couple2 => [man_b1, woman_b1]}
 man_a1 = Person.new("Thorpe", "Male", 1)
 woman_a1 = Person.new("Jain", "Female", 1)
 man_b1 = Person.new("Rolb", "Male", 1)
 woman_b1 = Person.new("Fess", "Female", 1)
+generation1 = {:couple1 => [man_a1, woman_a1], :couple2 => [man_b1, woman_b1]}
 
 # Generation 2
-generation2 = {:couple1 => [man_a2, woman_a2]}
 man_a2 = Person.new("Durn", "Male", man_a1.number.to_i + woman_a1.number.to_i)
 woman_a2 = Person.new("Lok", "Female", man_b1.number.to_i + woman_b1.number.to_i)
+generation2 = {:couple1 => [man_a2, woman_a2]}
 
 # Generation 3
-generation2 = {:individual => [man_a3]}
 man_a3 = Person.new("Wince", "Male", man_a2.number.to_i + woman_a2.number.to_i)
+generation2 = {:individual => [man_a3]}
 
 # Sanity check
 puts man_a1.id
