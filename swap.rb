ecdba8c491435773fb068867189002abbb4ba91e
rallen-fiksu <rallen@fiksu.com>
2014-05-12 20:33:06 -0400

    ready to start testing child swap

--- swap.rb
+++ swap.rb
@@ -1,2 +1,32 @@
 #!/usr/bin/ruby
 # Genetic swap
+
+class Gpair
+
+	attr_accessor :pair
+
+	def initialize (pair)
+		@pair = pair
+	end
+
+	def id
+		puts "Gene pair: #{pair}"
+	end
+
+	def self.swap
+  		flip = 1 + rand(2)
+    	if flip == 2 
+        	puts "Two"
+    	else
+        	puts "One"
+    	end
+	end 
+end
+
+man1 = Gpair.new("Ab")
+woman1 = Gpair.new("Ab")
+
+print "Father: "
+puts man1.id
+print "Mother: "
+puts woman1.id
