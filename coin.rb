07302ef49eed1242db3f2b37bcf124ccaab01be6
rallen-fiksu <rallen@fiksu.com>
2014-05-12 20:16:24 -0400

    trying a second more concise version. not working yet

--- coin.rb
+++ coin.rb
@@ -1,4 +1,25 @@
 #!/usr/bin/ruby
 # Genetic coin flip
 
+# Method 1
+puts "Type a the number of flips"
+x = gets.chomp.to_i
 
+=begin
+1.upto(x) do |i|
+	if rand.round == 0
+  		coin = "One"
+	else
+  		coin = "Two"
+	end
+	puts "#{i}: #{coin}"
+end
+=end
+
+# Method 2
+flip = Random.new
+
+1.upto(x) do |i|
+	flip.rand(2)
+	puts "#{i}: #{flip}"
+end
