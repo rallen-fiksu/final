c1009185a7777cc6a00c3a32432c1a69d9d7aa0d
rallen-fiksu <rallen@fiksu.com>
2014-05-13 19:33:24 -0400

    Method appears to mostly work. However, the child.id method is not yielding what it should. So... it kinda doesnt work either...

--- swap.rb
+++ swap.rb
@@ -31,6 +31,7 @@ def inherit(a,b)
     else
       y = b.pair[0,1]
     end
+    puts x + y
 end
 
 man1 = Gpair.new("Ab")
@@ -41,33 +42,6 @@ puts man1.id
 print "Mother: "
 puts woman1.id
 
-child1 = Gpair.new(inherit(man1,woman1))
 print "Child: "
+child1 = Gpair.new(inherit(man1,woman1))
 puts child1.id
-
-
-
-
-
-
-=begin
-# Gene inheritance + make child
-# This is a hard coded method. Not suitable for larger populations.
-x = "P"
-flip = 1 + rand(2)
-    if flip == 2 
-       	x = man1.pair[1,1]
-   	else
-       	x = man1.pair[0,1]
-   	end
-y = "P"
-flip = 1 + rand(2)
-    if flip == 2 
-       	y = woman1.pair[1,1]
-   	else
-       	y = woman1.pair[0,1]
-   	end
-child1 = Gpair.new(x + y)
-print "Child gene pair: "
-puts child1.id
-=end
